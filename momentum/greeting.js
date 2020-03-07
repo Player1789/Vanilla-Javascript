3128e7ee84fa38c9dae1cdb1c5285c177cc0576b
Player1789 <jhoon5245@gmail.com>
2020-03-08 12:16:20 +1300

    #3-4. Saving the user name part 2

--- momentum/greeting.js
+++ momentum/greeting.js
@@ -5,6 +5,28 @@ const form = document.querySelector('.js-form'),
 const USER_LS = 'currentUser',
 	SHOWING_CN = 'showing';
 
+function saveName(text) {
+	localStorage.setItem(USER_LS, text);
+}
+
+function handleSubmit(event) {
+	event.preventDefault();
+	const currentValue = input.value;
+	if (currentValue === '') {
+		askForName();
+	} else {
+		paintGreeting(currentValue);
+		saveName(currentValue);
+	}
+}
+
+function askForName() {
+	form.classList.add(SHOWING_CN);
+	greeting.classList.remove(SHOWING_CN);
+	const currentValue = input.value;
+	form.addEventListener('submit', handleSubmit);
+}
+
 function paintGreeting(text) {
 	form.classList.remove(SHOWING_CN);
 	greeting.classList.add(SHOWING_CN);
@@ -14,7 +36,7 @@ function paintGreeting(text) {
 function loadName() {
 	const currentUser = localStorage.getItem(USER_LS);
 	if (currentUser === null) {
-		console.log('tada');
+		askForName();
 	} else {
 		paintGreeting(currentUser);
 	}
